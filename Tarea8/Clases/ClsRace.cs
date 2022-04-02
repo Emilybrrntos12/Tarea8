58124dda2334b5b3b428250569aeabf6b92b7377
Emily Barrientos <Emily Barrientos@LAPTOP-MLHRD9JT>
2022-04-01 20:54:55 -0600

    Agregar archivos de proyecto.

---
+++ Tarea8/Clases/ClsRace.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea8.Clases
+{
+    internal class ClsRace
+    {
+        private carro CarroA;
+        private carro CarroB;
+        private carro CarroC;
+
+
+        public void PedidrDatos()
+        {
+
+            CarroA = GetData();
+            Console.WriteLine("\nAhora datos del B");
+            CarroB = GetData();
+            Console.WriteLine("\nAhora datos del C");
+            CarroC = GetData();
+        }
+
+        public void IniciarCarrera()
+        {
+            string msgCarroA, msgCarroB, msgCarroC;
+            Random velRamdom = new Random();
+
+
+            msgCarroA = CarroA.EncenderMotor();
+            msgCarroB = CarroB.EncenderMotor();
+            msgCarroC = CarroC.EncenderMotor();
+            Console.WriteLine("\n\tINICIA LA CARREA");
+            Console.WriteLine($"{CarroA.Marca} {msgCarroA}");
+            Console.WriteLine($"{CarroB.Marca} {msgCarroB}");
+            Console.WriteLine($"{CarroC.Marca} {msgCarroC}");
+
+            for (int i = 0; i < 10; i++)
+            {
+                int acelerar; ;
+                acelerar = velRamdom.Next(1, 21);
+                msgCarroA = CarroA.acelerar(acelerar);
+                acelerar = velRamdom.Next(1, 21);
+                msgCarroB = CarroB.acelerar(acelerar);
+                acelerar = velRamdom.Next(1, 21);
+                msgCarroC = CarroC.acelerar(acelerar);
+                Console.WriteLine($"{i + 1}- {CarroA.owner} {msgCarroA} // {CarroB.owner} {msgCarroB} // {CarroC.owner} {msgCarroC}");
+            }
+
+            if ((CarroA.GetVelocidadActual() > CarroB.GetVelocidadActual()) && (CarroA.GetVelocidadActual() > CarroC.GetVelocidadActual()))
+            {
+                Console.WriteLine($"GANO {CarroA.owner}!!");
+
+            }
+            else if ((CarroB.GetVelocidadActual() > CarroA.GetVelocidadActual()) && (CarroB.GetVelocidadActual() > CarroC.GetVelocidadActual()))
+            {
+                Console.WriteLine($"GANO {CarroB.owner}!!");
+            }
+            else if ((CarroC.GetVelocidadActual() > CarroA.GetVelocidadActual()) && (CarroC.GetVelocidadActual() > CarroB.GetVelocidadActual()))
+            {
+                Console.WriteLine($"GANO {CarroC.owner}!!");
+            }
+        }
+
+        private carro GetData()
+        {
+            string marca;
+            //int modelo;
+            int velocidad = 300;
+            string propietario;
+            carro carrotemporal;
+
+            Console.WriteLine("Ingrese marca");
+            marca = Console.ReadLine();
+
+            //Console.WriteLine("Ingrese modelo");
+            //modelo = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Ingrese velocidad max");
+            //velocidad = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"La maxima velocidad es de {velocidad}");
+
+            Console.WriteLine("Ingrese propietario");
+            propietario = Console.ReadLine();
+            carrotemporal = new carro(marca, velocidad, propietario);
+            return carrotemporal;
+        }
+
+    }
+}
---
+++ Tarea8/Clases/carro.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea8.Clases
+{
+    internal class carro
+    {
+        public string Marca { get; }
+        public int Modelo { get; }
+        public String Color { get; set; }
+
+        public string owner { get; set; }
+
+        private int Encendido = 0;
+        private int velocidad_actual = 0;
+
+        public int MAXVELOCIDAD { get; }
+
+        public carro(string _marca, int _modelo, string _color, int _maxvel)
+        {
+            Marca = _marca;
+            Modelo = _modelo;
+            Color = _color;
+            Encendido = 0;
+            MAXVELOCIDAD = _maxvel;
+            this.velocidad_actual = 0;
+
+        }
+
+        //contruir 2do constructor
+        public carro(string _marca, int _maxvel, string _owner)
+        {
+            Marca = _marca;
+            //Modelo = _modelo;
+            MAXVELOCIDAD = 300;
+            owner = _owner;
+            this.velocidad_actual = 0;
+        }
+        public int GetVelocidadActual()
+        {
+            return velocidad_actual;
+        }
+
+        public string EncenderMotor()
+        {
+            if (Encendido == 0)
+            {
+
+                Encendido = 1;
+                this.velocidad_actual = 0;
+                return "RUN RUN RUN RUN, LISTOS PARA CORRER";
+            }
+            else
+            {
+                return "Orale!!!! ya esta enciendio tu carro";
+            }
+        }
+
+        public string acelerar1()
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"no puedo acelerar con el ccarro apagado";
+                return mensaje;
+            }
+
+            velocidad_actual += 10;
+            if (velocidad_actual <= MAXVELOCIDAD)
+            {
+                mensaje = $"Vas a {velocidad_actual} KPH";
+            }
+            else
+            {
+                velocidad_actual = MAXVELOCIDAD;
+                mensaje = $"{velocidad_actual}KPH Aguas vas muy rapido y ya llegaste al tope";
+            }
+
+            return mensaje;
+        }
+
+        public string acelerar(int CuantosKPH)
+        {
+            string mensaje = "";
+            if (Encendido == 0)
+            {
+                mensaje = $"No puedo acelerar con el carro apagado";
+                return mensaje;
+            }
+
+            velocidad_actual += CuantosKPH;
+            if (velocidad_actual <= MAXVELOCIDAD)
+            {
+                mensaje = $"{velocidad_actual} KPH";
+            }
+            else
+            {
+                velocidad_actual = MAXVELOCIDAD;
+                mensaje = $"{velocidad_actual}KPH Aguas rapido y furioso, vas muy rapido DETENTE";
+            }
+
+            return mensaje;
+        }
+
+        public string modelo()
+        {
+            string mensaje = "";
+
+            return mensaje;
+        }
+    }
+
+}
